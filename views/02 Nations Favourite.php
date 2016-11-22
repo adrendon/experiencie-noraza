4818ea33e497671961a55ed78046768498d93dc2
alexis rendón <alexis9229@hotmail.com>
2016-11-22 00:17:39 -0500

    changes

--- views/02 Nations Favourite.php
+++ views/02 Nations Favourite.php
@@ -2,38 +2,41 @@
 
 		<div class="background-colour"></div>
 
-		<img src="https://costa-experience.s3.amazonaws.com/img/backgrounds/a3cfe5.png" style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;z-index: 1;" />
+		<!-- <img src="https://costa-experience.s3.amazonaws.com/img/backgrounds/a3cfe5.png" style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;z-index: 1;" /> -->
 
 		<ul class="scene">
 			<li class="box box1 layer" data-depth="-0.2">
  				<!-- <div class="dinosaur sprite" data-src="dinosaur.png" data-width="114" data-height="117" data-cols="71" data-delay="6000" data-startdelay="4000"></div>			
  				<div class="grass"></div>
  				<div class="pipe sprite" data-src="pipe.png" data-width="472" data-height="225" data-cols="17" data-delay="7000"></div> -->
-				<img src="http://i.imgur.com/m9NS5Op.png" class="level">
+				<img src="https://adrendon.github.io/experiencie-noraza/pagweb/level-5.png" class="level">
 				<!-- <div class="cloud-top"></div> -->
 			</li>
-			<li class="box box2 layer" data-depth="0.00">
+			<li class="box box2 layer" data-depth="0.2">
 				<!--<img src="https://costa-experience.s3.amazonaws.com/img/02 Nations Favourite/man.png" alt="" class="man">-->
 				<!-- <div class="man man1 sprite" data-src="man.png" data-width="134" data-height="230" data-cols="49" data-framerate="24" data-delay="4000"></div> -->
-				<img src="http://i.imgur.com/7qvvSwd.png" class="level">
+				<img src="https://adrendon.github.io/experiencie-noraza/pagweb/level-4.png" class="level">
 				<!-- <div class="cloud-middle"></div> -->
 			</li>
-			<li class="box box3 layer" data-depth="0.2">
+			<li class="box box3 layer" data-depth="0.4">
 				<!-- <div class="tree dynamic-size">
 					<div class="tree-bole"></div>
 					<div class="tree-foilage"></div>
 				</div>
 				<div class="bunting sprite" data-src="cafe-bunting.png" data-width="1002" data-height="647" data-framerate="24" data-cols="24"></div>
 				<div class="cafe-people-shadows sprite" data-src="window-people-sprite.png" data-width="286" data-height="115" data-cols="49" data-delay="2000"></div> -->
-				<img src="http://i.imgur.com/K1558DH.png" class="level">
+				<img src="https://adrendon.github.io/experiencie-noraza/pagweb/level-3.png" class="level">
 			</li>
-			<li class="box box4 layer" data-depth="1">
+			<li class="box box4 layer" data-depth="0.6">
 				<!-- <div class="car car-1 sprite" data-src="car.png" data-width="77" data-height="42" data-cols="15"></div>
 				<div class="car car-2 sprite" data-src="car.png" data-width="77" data-height="42" data-cols="15"></div>
 				<div class="car car-3 sprite" data-src="car.png" data-width="77" data-height="42" data-cols="15"></div>
 				<div class="lorry sprite" data-src="lorry.png" data-width="93" data-height="61" data-cols="16"></div> -->
-				<img src="http://i.imgur.com/bkh5tOX.jpg" class="level">
+				<img src="https://adrendon.github.io/experiencie-noraza/pagweb/level-2.png" class="level">
 				<!-- <div class="cloud-bottom"></div> -->
+			</li>
+			<li class="box box5 layer" data-depth="1.0">
+				<img src="https://adrendon.github.io/experiencie-noraza/pagweb/level-1.png" class="level">
 			</li>
 			<!-- <li class="clouds">
 				<img src="https://costa-experience.s3.amazonaws.com/img/global/cloud-1-164x61.png" class="cloud-1-164x61">
